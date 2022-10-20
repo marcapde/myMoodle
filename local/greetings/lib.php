c75d06f28debb4a06bef8c56ba425af9f94d7159
Marc Capde <capasc5@gmail.com>
2022-10-20 13:31:37 +0200

    Add settings page

--- local/greetings/lib.php
+++ local/greetings/lib.php
@@ -60,29 +60,31 @@ function local_greetings_get_greeting($user) {
  * @param navigation_node $frontpage Node representing the front page in the navigation tree.
  */
 function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
-    if (isloggedin() && !isguestuser()) {
-        $frontpage->add(
-            get_string('pluginname', 'local_greetings'),
-            new moodle_url('/local/greetings/index.php'),
-            navigation_node::TYPE_CUSTOM,
-            null,
-            null,
-            new pix_icon('t/message', '')
-        );
-    }
+    if (get_config('local_greetings', 'showinnavigation')){
+        if(isloggedin() && !isguestuser()) {
+            $frontpage->add(
+                get_string('pluginname', 'local_greetings'),
+                new moodle_url('/local/greetings/index.php'),
+                navigation_node::TYPE_CUSTOM,
+                null,
+                null,
+                new pix_icon('t/message', '')
+            );
+        }
+    } 
 }
 
-// /**
-//  * Add link to index.php into navigation drawer.
-//  *      
-//  * @param global_navigation $root Node representing the global navigation tree.
-//  */
+/**
+ * Add link to index.php into navigation drawer.
+ *      
+ * @param global_navigation $root Node representing the global navigation tree.
+ */
 // function local_greetings_extend_navigation(global_navigation $root) {
 //     $node = navigation_node::create(
 //         get_string('sayhello', 'local_greetings'),
 //         new moodle_url('/local/greetings/index.php')
 //     );
 
-//     $node->showinflatnavigation = true;
+//     $node->showinflatnavigation = get_config('local_greetings', 'showinnavigation');
 //     $root->add_node($node);
 // }

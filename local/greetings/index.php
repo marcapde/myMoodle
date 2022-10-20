3d71bf088cdd81301d97417736221f5ec5302280
Marc Capde <capasc5@gmail.com>
2022-10-20 11:34:35 +0200

    Add capability check 2

--- local/greetings/index.php
+++ local/greetings/index.php
@@ -39,6 +39,21 @@ if (isguestuser()) {
 }
 // To check if a user has the capability to post.
 $allowpost = has_capability('local/greetings:postmessages', $context);
+$allowread = has_capability('local/greetings:viewmessages', $context);
+$deletepost = has_capability('local/greetings:deleteownmessage', $context);
+$deleteanypost = has_capability('local/greetings:deleteanymessage', $context);
+$action = optional_param('action', '', PARAM_TEXT);
+
+if ($action == 'del') {
+    $id = required_param('id', PARAM_TEXT);
+
+    if ($deleteanypost || $deletepost) {
+        $params = array('id' => $id);
+
+        $DB->delete_records('local_greetings_messages', $params);
+    }
+}
+
 $messageform = new local_greetings_message_form();
 if ($data = $messageform->get_data()) {
     require_capability('local/greetings:postmessages', $context);
@@ -64,29 +79,41 @@ if (isloggedin()) {
 if ($allowpost) {
     $messageform->display();
 }
+if ($allowread){
+    $userfields = \core_user\fields::for_name()->with_identity($context);
+    $userfieldssql = $userfields->get_sql('u');
 
-$userfields = \core_user\fields::for_name()->with_identity($context);
-$userfieldssql = $userfields->get_sql('u');
+    $sql = "SELECT m.id, m.message, m.timecreated, m.userid {$userfieldssql->selects}
+            FROM {local_greetings_messages} m
+        LEFT JOIN {user} u ON u.id = m.userid
+        ORDER BY timecreated DESC";
+        $messages = $DB->get_records_sql($sql);echo $OUTPUT->box_start('card-columns');
 
-$sql = "SELECT m.id, m.message, m.timecreated, m.userid {$userfieldssql->selects}
-          FROM {local_greetings_messages} m
-     LEFT JOIN {user} u ON u.id = m.userid
-      ORDER BY timecreated DESC";
-
-$messages = $DB->get_records_sql($sql);echo $OUTPUT->box_start('card-columns');
-
-foreach ($messages as $m) {
-    echo html_writer::start_tag('div', array('class' => 'card'));
-    echo html_writer::start_tag('div', array('class' => 'card-body'));
-    // echo html_writer::tag('p', $m->message, array('class' => 'card-text')); without sanitasing
-    echo html_writer::tag('p', format_text($m->message, FORMAT_PLAIN), array('class' => 'card-text')); // Sanitized.
-    echo html_writer::tag('p', get_string('postedby', 'local_greetings', $m->firstname), array('class' => 'card-text'));
-    echo html_writer::start_tag('p', array('class' => 'card-text'));
-    echo html_writer::tag('small', userdate($m->timecreated), array('class' => 'text-muted'));
-    echo html_writer::end_tag('p');
-    echo html_writer::end_tag('div');
-    echo html_writer::end_tag('div');
+    foreach ($messages as $m) {
+        echo html_writer::start_tag('div', array('class' => 'card'));
+        echo html_writer::start_tag('div', array('class' => 'card-body'));
+        // echo html_writer::tag('p', $m->message, array('class' => 'card-text')); without sanitasing
+        echo html_writer::tag('p', format_text($m->message, FORMAT_PLAIN), array('class' => 'card-text')); // Sanitized.
+        echo html_writer::tag('p', get_string('postedby', 'local_greetings', $m->firstname), array('class' => 'card-text'));
+        echo html_writer::start_tag('p', array('class' => 'card-text'));
+        echo html_writer::tag('small', userdate($m->timecreated), array('class' => 'text-muted'));
+        echo html_writer::end_tag('p');
+        if ($deleteanypost || ($deletepost && $m->userid == $USER->id)) {
+            echo html_writer::start_tag('p', array('class' => 'card-footer text-center'));
+            echo html_writer::link(
+                new moodle_url(
+                    '/local/greetings/index.php',
+                    array('action' => 'del', 'id' => $m->id)
+                ),
+                $OUTPUT->pix_icon('t/delete', '') . get_string('delete')
+            );
+            echo html_writer::end_tag('p');
+        }
+        echo html_writer::end_tag('div');
+        echo html_writer::end_tag('div');
+    }
 }
+
 
 echo $OUTPUT->box_end();
 
